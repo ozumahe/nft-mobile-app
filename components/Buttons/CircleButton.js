f4cbcf28133517f15f5b11f528699aea23d58406
ozumahe <ozumahe@gmail.com>
2022-08-01 13:44:50 +0100

    initail commit

---
+++ components/Buttons/CircleButton.js
@@ -0,0 +1,31 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+
+import { COLORS, SIZES, FONTS, SHADOWS } from "../../constants";
+
+const CircleButton = ({ imgUrl, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: "absolute",
+        borderRadius: SIZES.extraLarge,
+        alignItems: "center",
+        justifyContent: "center",
+        ...SHADOWS.light,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Image
+        source={imgUrl}
+        resizeMode="contain"
+        style={{ width: 24, height: 24 }}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default CircleButton;
---
+++ components/NFTCard.js
@@ -0,0 +1,44 @@
+import { View, Text, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+
+import CircleButton from "../components/Buttons/CircleButton";
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
+  const handlePress = () => {};
+
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ height: 250, width: "100%" }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+      </View>
+      <CircleButton
+        imgUrl={assets.heart}
+        right={10}
+        top={10}
+        handlePress={handlePress}
+      />
+    </View>
+  );
+};
+
+export default NFTCard;
